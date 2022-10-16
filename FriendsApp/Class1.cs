2bff3e3b9bf4726c51de86e57736f0116ad5ffdb
Tinaskj <106248161+Tinaskj@users.noreply.github.com>
2022-10-16 12:27:24 +0200

    Initial commit

---
+++ FriendsApp/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendsApp
+{
+    internal class Profile
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public int Age { get; set; }
+        public bool Male { get; set; }
+        private List<Profile> Friends { get; set; }
+
+        public Profile(string name, string id)
+        {
+            Name = name;
+            Id = id;
+            Friends = new List<Profile>();
+            
+        }
+
+        public void PrintProfileInfo()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Age: {Age}");
+            Console.WriteLine($"Male: {Male}");
+            ShowFriends();
+        }
+        
+        public void ShowFriends()
+        {
+            foreach (var friend in Friends)
+            {
+                Console.WriteLine($"{friend.Name} {friend.Id}");
+            }
+        }
+        public void AddFriend(Profile friendToAdd)
+        {
+            Friends.Add(friendToAdd);
+        }
+        public void RemoveFriend(Profile friendToRemove)
+        {
+            Friends.Remove(friendToRemove);
+        }
+    }
+}
---
+++ FriendsApp/Class2.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendsApp
+{
+    class SocialNetwork
+    {
+        private List<Profile> ListOfUsers { get; set; }
+        private Profile loggedInProfile;
+        public SocialNetwork()
+        {
+            loggedInProfile = new Profile("Davina", "0");
+
+            ListOfUsers = new List<Profile>();
+            ListOfUsers.Add(new Profile("Jamie", "1"));
+            ListOfUsers.Add(new Profile("Rose", "2"));
+            ListOfUsers.Add(new Profile("Lucy", "3"));
+            ListOfUsers.Add(new Profile("Lucifer", "4"));
+
+            while (true)
+            {
+                PrintMenu();
+                var userInput = Console.ReadLine();
+                HandleMenuOption(userInput);
+            }
+           
+        }
+
+        private void ShowUsers()
+        {
+            foreach (var user in ListOfUsers)
+            {
+                Console.WriteLine($"{user.Name} { user.Id}");
+            }
+        }
+        private void PrintMenu()
+        {
+            Console.WriteLine("Welcome to Friendface");
+            Console.WriteLine("1. Add friend");
+            Console.WriteLine("2. Remove friend");
+            Console.WriteLine("3. Print friends");
+            Console.WriteLine("4. Show friend info");
+        }
+
+        private void HandleMenuOption(string menuOption)
+        {
+            if(menuOption == "1")
+            {
+                Console.WriteLine("MenuOption 1: Add friend: ");
+                ShowUsers();
+                Console.WriteLine("Choose the id of the user you want to add");
+                var chosenUserId = Console.ReadLine();
+                var userProfile = GetUserProfile(chosenUserId);
+                loggedInProfile.AddFriend(userProfile);
+            }
+            if(menuOption == "2")
+            {
+                loggedInProfile.ShowFriends();
+                Console.WriteLine("Choose which friend to remove: ");
+                var friendId = Console.ReadLine();
+                var profileToRemove = GetUserProfile(friendId);
+                loggedInProfile.RemoveFriend(profileToRemove);
+            }
+            if(menuOption == "3")
+            {
+                loggedInProfile.ShowFriends();
+            }
+            if(menuOption == "4")
+            {
+                loggedInProfile.ShowFriends();
+                var friendId = Console.ReadLine();
+                var profile = GetUserProfile(friendId);
+                profile.PrintProfileInfo();
+            }
+        }
+        private Profile GetUserProfile(string id)
+        {
+            return ListOfUsers.FirstOrDefault(user => user.Id == id);
+        }
+    }
+}
